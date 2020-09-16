d13aa870786ac828b01cbcf489dc1e1df3cd7cb8
Maioon-png <mi_mkb.12_5@icloud.com>
2020-09-16 15:38:36 +0900

    first commite

--- Swift5Bokete/ViewController.swift
+++ Swift5Bokete/ViewController.swift
@@ -7,14 +7,37 @@
 //
 
 import UIKit
+//ライブラリ記述
+import Alamofire
+import SwiftyJSON
+import SDWebImage
+//許可画面
+import Photos
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var odaiImageView: UIImageView!
+    
+    @IBOutlet weak var commentTextView: UITextView!
+    
+    @IBOutlet weak var searchTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        commentTextView.layer.cornerRadius = 20.0
+        PHPhotoLibrary.requestAuthorization { (status) in
+            switch(status){
+                case .authorized: break
+                case .denied: break
+                case .notDetermined: break
+                case .restricted: break
+            }
+        }
+            
+        
     }
-
+//
 
 }
 
