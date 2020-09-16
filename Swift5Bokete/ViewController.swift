95281d7f2c8b9c09881ddade839d217dd9443e37
Maioon-png <mi_mkb.12_5@icloud.com>
2020-09-16 17:49:01 +0900

    Update ViewController.swift

--- Swift5Bokete/ViewController.swift
+++ Swift5Bokete/ViewController.swift
@@ -22,10 +22,14 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var searchTextField: UITextField!
     
+    var count = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        //imageViewの角丸
         commentTextView.layer.cornerRadius = 20.0
+        //アルバムの許可
         PHPhotoLibrary.requestAuthorization { (status) in
             switch(status){
                 case .authorized: break
@@ -35,9 +39,80 @@ class ViewController: UIViewController {
             }
         }
             
+        getImages(keyword: "funny")
         
     }
-//
-
+//検索キーワードの値をもとに画像をひっぱってくる
+//pixabay.comから
+    
+    func getImages(keyword:String){
+        //APIkey
+        let url = "https://pixabay.com/api/?key=18328124-1c5ca24b67cda9110ef649247&q=\(keyword)"
+        //Alamfireを使ってhttpリクエストを投げる
+        //~in　はクロージャーという⇨selfを使う？
+        AF.request(url, method: .get, parameters: nil, encoding: JSONEncoding.default).responseJSON{ (response) in
+            
+            switch response.result{
+            
+            //レスポンスが正常に行われていたとき
+            case .success:
+                let json:JSON = JSON(response.data as Any)
+                var imageString = json["hits"][self.count]["webformatURL"].string
+                
+                //もう配列の次がないよ〜って場合
+                if imageString == nil{
+                    
+                    imageString = json["hits"][0]["webformatURL"].string
+                    self.odaiImageView.sd_setImage(with: URL(string: imageString!), completed: nil)
+                    
+                }else{//配列の次がまだある場合
+                 
+                    self.odaiImageView.sd_setImage(with: URL(string: imageString!), completed: nil)
+                    
+                }
+                    
+            case .failure(let error):
+                print(error)
+                
+            }
+        }
+    }
+    
+    //次のお題へ
+    @IBAction func nextOdai(_ sender: Any) {
+        
+        count = count + 1
+        //検索ワード欄が空だったら
+        if searchTextField.text == ""{
+            getImages(keyword: "funny")
+        }else{
+            getImages(keyword: searchTextField.text!)
+        }
+    }
+    
+    //検索ボタンが押されたとき
+    @IBAction func serachAction(_ sender: Any) {
+        
+        self.count = 0
+        
+        if searchTextField.text == ""{
+           getImages(keyword: "funny")
+       }else{
+           getImages(keyword: searchTextField.text!)
+       }
+    }
+    
+    @IBAction func next(_ sender: Any) {
+        performSegue(withIdentifier: "next", sender: nil)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        //ShareVCを変数化
+        let shareVC = segue.destination as? ShareViewController
+        shareVC?.commentString = commentTextView.text
+        shareVC?.resultImage = odaiImageView.image!
+        
+    }
+    
 }
 
